8c9260703561c8a05e9cbf4ad9f4d5383b318644
Jonah Meijers <jonahmeijers97@gmail.com>
2020-11-06 08:57:40 +0000

    fix: remove tailwindcss from postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 const presetEnv = require('postcss-preset-env')
 const autoprefixer = require('autoprefixer')
-const tailwindcss = require('tailwindcss')
 
 module.exports = {
-  plugins: [tailwindcss(), autoprefixer(), presetEnv()],
+  plugins: [autoprefixer(), presetEnv()],
 }
